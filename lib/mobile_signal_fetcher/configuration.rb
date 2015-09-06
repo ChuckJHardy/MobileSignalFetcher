1c8730d58e84b246d7dadf59976eb2a81adaa97d
Charles J Hardy <chuckjhardy@gmail.com>
2015-09-06 12:19:30 +0100

    New Configuration Options

--- lib/mobile_signal_fetcher/configuration.rb
+++ lib/mobile_signal_fetcher/configuration.rb
@@ -12,12 +12,14 @@ class MobileSignalFetcher
     private
 
     class Configuration
-      attr_accessor :domain, :api_key, :verbose
+      attr_accessor :domain, :api_key, :verbose, :log, :logger
 
       def initialize
         self.domain = 'http://api.opensignal.com/v2/'
         self.api_key = nil
         self.verbose = false
+        self.log = false
+        self.logger = Logger.new(STDOUT)
       end
     end
   end
